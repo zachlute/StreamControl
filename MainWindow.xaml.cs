30854ca15b8bbe06bcb0ead397238878f49cea2e
Zach Lute <zach.lute@gmail.com>
2016-11-20 18:52:52 -0800

    Initial commit of timeline control with some basic drawing.

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -14,10 +14,77 @@ using System.Windows.Navigation;
 using System.Windows.Shapes;
 
 namespace StreamControl {
+    public class Timeline {
+        public IEnumerable<Revision> Revisions
+        {
+            get;
+            set;
+        }
+
+        public Timeline () {
+            var time = DateTime.Now;
+            Revisions = new[] {
+                new Revision(time.AddMinutes(-5), "Source created", true, true, false),
+                new Revision(time.AddMinutes(-4), "Dest created", false, true, false),
+                new Revision(time.AddMinutes(-3), "Change in source", true),
+                new Revision(time.AddMinutes(-2), "Change in dest", false), 
+                new Revision(time.AddMinutes(-1.5), "Change in source 2", true),
+                new Revision(time.AddMinutes(-1), "Change in source 3", true),
+                new Revision(time.AddMinutes(-0.5), "Change in dest 2", false),
+                new Revision(time.AddMinutes(0), "Change in dest 3", false),
+                new Revision(time.AddMinutes(1), "Current", false, isCurrent: true)
+            };
+        }
+    }
+
+    public class Revision {
+        public bool IsSource
+        {
+            get;
+        }
+
+        public bool IsDest => !IsSource;
+
+        public bool IsCurrent
+        {
+            get;
+        }
+
+        public bool IsCreate
+        {
+            get;
+        }
+
+        public string Message
+        {
+            get;
+            set;
+        }
+
+        public DateTime Time
+        {
+            get;
+        }
+
+        public Revision(DateTime time, string message, bool isSource, bool isCreate = false, bool isCurrent = false) {
+            Time = time;
+            Message = message;
+            IsSource = isSource;
+            IsCreate = isCreate;
+            IsCurrent = isCurrent;
+        }
+    }
+
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
     public partial class MainWindow : Window {
+
+        public Timeline Timeline {
+            get;
+            set;
+        } = new Timeline();
+
         public MainWindow () {
             InitializeComponent();
         }
